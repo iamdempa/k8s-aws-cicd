b0136c74f2360b554bb0957a53be27cf256221e5
iamdempa <banukajananathjayarathna@gmail.com>
2020-02-28 18:47:29 +0530

    test

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -15,7 +15,16 @@ resource "aws_instance" "kubernetes-instances" {
   ami = "${var.ec2-ami}"
   instance_type = "${var.ec2-type}"
 
-  user_data = "${file(install_ansible.sh)}"
+  # user_data = "${file(install_ansible.sh)}"
+
+	user_data = << EOF
+		#! /bin/bash
+    sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
 
   tags = {
       Name = "${var.ec2-name}"
