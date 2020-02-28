21287a457978c7fc6a877691c47c5cbcac818d94
iamdempa <banukajananathjayarathna@gmail.com>
2020-02-28 14:35:55 +0530

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+
+
 module "kubernetes-instances" {
+
     source = "./module/ec2"
-    ami = "ami-07ebfd5b3428b6f4d"
-    instance_type = "t2.medium"
+    ec2-ami = "ami-07ebfd5b3428b6f4d"
+    ec2-type = "t2.medium"
     ec2-name = "kube-master"
+
 }
