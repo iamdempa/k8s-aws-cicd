c7ea111c75170c935a5355f75ce2c6df8abfe5cd
iamdempa <banukajananathjayarathna@gmail.com>
2020-03-02 17:42:54 +0530

    test

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "sg-kube-master-allow-ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
+  egress {
     from_port = 0
     to_port = 65535
     protocol = "tcp"
