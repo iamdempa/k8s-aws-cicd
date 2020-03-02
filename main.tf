78fa7b75171baf45d9baa033057e07cafe4cc22c
iamdempa <banukajananathjayarathna@gmail.com>
2020-03-02 17:23:16 +0530

    test

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_security_group" "kube-master-allow-ssh" {
   ingress {
     from_port = 22
     to_port = 22
-    protocol = "ssh"
+    protocol = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
   }
