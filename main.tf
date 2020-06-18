33f9696fd5334d3af366e81f0375fe82823b1f47
iamdempa <banukajananathjayarathna@gmail.com>
2020-06-18 20:26:04 +0530

    test

--- main.tf
+++ main.tf
@@ -152,7 +152,7 @@ resource "aws_route_table" "kube-master-rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.kubernetes-igw.id}"
+    # gateway_id = "${aws_internet_gateway.kubernetes-igw.id}"
   }
 
   tags = {
