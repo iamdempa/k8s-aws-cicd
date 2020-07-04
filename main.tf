0809197e33836a23674890068d892fedc033376d
iamdempa <banukajananathjayarathna@gmail.com>
2020-07-04 16:12:11 +0530

    test

--- main.tf
+++ main.tf
@@ -169,7 +169,7 @@ EOD
   }
 
   provisioner "local-exec" {
-    command = "for i in $(seq 1 ${var.minion-count}); do echo ${aws_instance.kubernetes_master.public_ip} > /etc/ansible/machan; done"
+    command = "for i in $(seq 1 ${var.minion-count}); do echo ${aws_instance.kubernetes_minion.*.public_ip} > /etc/ansible/machan; done"
   }
  }
 
